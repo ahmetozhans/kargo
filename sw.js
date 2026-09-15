3e211c2bac0bdf0db5582f7086be4e6bad4b01e8
Ahmet Özhan <ahmetozhan42@gmail.com>
2026-09-15 21:14:09 +0300

    chore: cache rebuilt mobile CSS

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'kargo-shell-v10';
+const CACHE_NAME = 'kargo-shell-v11';
 const APP_SHELL = [
   './',
   './index.html',
-  './app-ui.css',
+  './app-ui-v2.css',
   './app.js',
   './search.js',
   './map-shell.js',
